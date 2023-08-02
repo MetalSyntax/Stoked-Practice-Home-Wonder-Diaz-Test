b6f0e002c8e57b516c6116c407a0521b0f687f75
Wonder Diaz <wonderdxk@gmail.com>
2023-08-01 21:34:10 -0400

    1.0.1
    update project

---
+++ script.js
@@ -0,0 +1,119 @@
+// Initialize Swiper for the six-slider-swiper
+let sixSliderSwiper = new Swiper(".six-slider-swiper", {
+  // Enable lazy loading
+  lazy: true,
+  // Set number of slides to show
+  slidesPerView: 6,
+  // Set space between slides
+  spaceBetween: 30,
+  // Enable looping
+  loop: true,
+  // Set breakpoints for responsive design
+  breakpoints: {
+    280: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 25,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    },
+  },
+  // Enable navigation buttons
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Enable autoplay and set delay
+  autoplay: {
+    delay: 5000,
+  },
+  // Enable parallax effect
+  parallax: true,
+});
+
+// Initialize Swiper for the three-slider-swiper
+let threeSliderSwiper = new Swiper(".three-slider-swiper", {
+  // Enable lazy loading
+  lazy: true,
+  // Set number of slides to show
+  slidesPerView: 3,
+  // Set space between slides
+  spaceBetween: 30,
+  // Center the active slide
+  centeredSlides: true,
+  // Enable looping
+  loop: true,
+  // Set breakpoints for responsive design
+  breakpoints: {
+    280: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  // Enable pagination and set type
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  // Enable autoplay and set delay
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+// Initialize Swiper for the slider-full-swiper
+let sliderFullSwiper = new Swiper(".slider-full-swiper", {
+  // Enable lazy loading
+  lazy: true,
+  // Set number of slides to show
+  slidesPerView: 1,
+  // Set space between slides
+  spaceBetween: 30,
+  // Enable looping
+  loop: true,
+  // Enable scrollbar and hide it
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  // Enable autoplay and set delay
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+// Get the hamburguer button
+var hamburguerButton = document.querySelector(".hamburguer-button");
+// Get the mobile menu overlay
+var mobileMenuOverlay = document.querySelector(".mobile-menu-overlay");
+// Add click event listener to hamburguer button
+hamburguerButton.addEventListener("click", function () {
+  // Add "open" class to mobile menu overlay
+  mobileMenuOverlay.classList.add("open");
+});
+// Add click event listener to overlay area
+mobileMenuOverlay.addEventListener("click", function () {
+  // Remove "open" class from mobile menu overlay
+  mobileMenuOverlay.classList.remove("open");
+});
