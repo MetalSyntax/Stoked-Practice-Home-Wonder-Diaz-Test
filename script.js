8d72d07c1d729e647f0eb3449bb71e7b61d55722
Wonder Diaz <wonderdxk@gmail.com>
2023-08-02 12:54:35 -0400

    Update comments on JS and removed old tags

--- script.js
+++ script.js
@@ -118,28 +118,32 @@ mobileMenuOverlay.addEventListener("click", function () {
   mobileMenuOverlay.classList.remove("open");
 });
 
+// Selecting the top header element
 const topHeader = document.querySelector(".top-header");
+// Selecting the main header element
 const mainHeader = document.querySelector(".main-header");
+// Selecting the shipping free container element
 const shippingFreeContainer = document.querySelector(".shipping-free-container");
-
+// Function to handle the scroll event
 function handleScroll() {
+  // Adding the "fixed" class to the top header when scrolling past its offset position
   if (window.pageYOffset > topHeader.offsetTop) {
     topHeader.classList.add("fixed");
   } else {
     topHeader.classList.remove("fixed");
   }
-
+  // Adding the "fixed" class to the main header when scrolling past its offset position
   if (window.pageYOffset > mainHeader.offsetTop) {
     mainHeader.classList.add("fixed");
   } else {
     mainHeader.classList.remove("fixed");
   }
-
+  // Adding the "fixed" class to the shipping free container when scrolling past its offset position
   if (window.pageYOffset > shippingFreeContainer.offsetTop) {
     shippingFreeContainer.classList.add("fixed");
   } else {
     shippingFreeContainer.classList.remove("fixed");
   }
 }
-
+// Adding a scroll event listener to the window, calling the handleScroll function
 window.addEventListener("scroll", handleScroll);
